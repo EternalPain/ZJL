aa641a02404b322123b86eab434b67bc3f9cdd85
龍哥 <1965786708@qq.com>
2020-11-27 13:33:13 +0800

    ZJL2.0beta28

--- ZJL2.0beta28/模块/规则.sh
+++ ZJL2.0beta28/模块/规则.sh
@@ -1,13 +1,11 @@
 echo
-if [[ `pgrep tiny` ]]; then
-	echo " tiny 已开启 √\n"
-elif [[ `pgrep clnc` ]]; then
-	echo " clnc 已开启 √\n"
-elif [[ `pgrep CProxy` ]]; then
-	echo " CProxy 已开启 √\n"
-elif [[ `pgrep localproxy` ]]; then
-	echo " localproxy 已开启 √\n"
-else echo " 什么都没开启 ×\n";fi
+for ZJL in tiny clnc CProxy localproxy; do
+    [[ `pgrep ${ZJL}` ]] && core="${ZJL}" && break
+done
+
+[[ ${ZJL} ]] && echo " ${ZJL} 已开启 √\n" || echo " 什么都没开启 ×\n"
+
+[[ ! ${ZJL} ]] && echo 1 && echo 2
 
 [[ `pgrep pdnsd` ]] && echo " pdnsd 已开启 √\n"
 
@@ -40,11 +38,6 @@ do
 	iptables -t filter -S ${ZJL}
 	echo "\n━━━━━━━━━━━━━━━"
 done
-
-
-echo "\n\n━━━━━ ipv4 路由表 ━━━━━\n"
-	ip rule show
-echo "\n━━━━━━━━━━━━━━━"
 
 echo "\n\n━━━━━ ipv6 路由表 ━━━━━\n"
 	ip -6 rule show
