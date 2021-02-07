76bcc747e65405445329243469881dd0772ec4a0
EternalPain <1965786708@qq.com>
2021-02-07 21:55:39 +0800

    修复点bug

--- ZJL2.0beta29/模块/规则.sh
+++ ZJL2.0beta29/模块/规则.sh
@@ -11,33 +11,35 @@ done
 
 [[ `pgrep redsocks2` ]] && echo " redsocks2 已开启 √\n"
 
-
+echo "━━━━━ iptables ━━━━━━\n"
 echo "━━━━━━━ nat ━━━━━━━"
-
 for ZJL in OUTPUT PREROUTING
 do
 	echo
 	iptables -t nat -S ${ZJL}
-	echo "\n━━━━━━━━━━━━━━━"
+	echo "\n━━━━━━━━━━━━━━━━━"
 done
 
 echo "\n\n━━━━━━ mangle ━━━━━━"
-
 for ZJL in OUTPUT FORWARD PREROUTING
 do
 	echo
 	iptables -t mangle -S ${ZJL}
-	echo "\n━━━━━━━━━━━━━━━"
+	echo "\n━━━━━━━━━━━━━━━━━"
 done
 
 echo "\n\n━━━━━━ filter ━━━━━━"
-
 for ZJL in OUTPUT FORWARD
 do
 	echo
 	iptables -t filter -S ${ZJL}
-	echo "\n━━━━━━━━━━━━━━━"
+	echo "\n━━━━━━━━━━━━━━━━━"
 done
+
+echo "\n\n━━━━━ ip6tables ━━━━━\n"
+echo "━━━━━━ mangle ━━━━━━\n"
+ip6tables -t mangle -S FORWARD
+echo "\n━━━━━━━━━━━━━━━━━"
 
 echo "\n\n━━━━━ ipv6 路由表 ━━━━━\n"
 	ip -6 rule show
