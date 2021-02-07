b49fc3db8f1566f7b284315fd1271a37ace310e0
EternalPain <1965786708@qq.com>
2021-02-07 14:02:03 +0800

    更新beta29

---
+++ ZJL2.0beta29/关闭.sh
@@ -0,0 +1,27 @@
+zjlPath="${0%/*}/*/ZJL"
+
+if [ -f ${zjlPath} ]; then
+    if [ ! -x ${zjlPath} ]; then
+        isNormal="permissionDenied"
+    fi
+else
+    isNormal="notFound"
+fi
+
+if [ ! ${isNormal} ]; then
+    ${zjlPath} -c -d
+else
+    echo "\n      __________________________\n\n"\
+          "              ZJL 2.0\n"\
+          "     __________________________\n"
+          
+    if [ "notFound" == ${isNormal} ]; then
+        echo "           找不到ZJL核心文件\n\n"\
+              "          请复制回模块文件夹\n\n"\
+              "          请修改权限为0777"
+    else
+        echo "           ZJL核心权限有问题\n\n"\
+              "          请修改权限为0777"
+    fi
+    echo "     __________________________\n"
+fi
